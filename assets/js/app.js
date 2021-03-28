e22de093542694954218da4aa6df87010dd72a19
andrea <git@andreaemmanuele.it>
2021-03-28 18:33:39 +0100

    chats done - still couldn't send messages inside chat

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,36 @@
 new Vue({
     el: "#root",
     data: {
-        onChat: 1,
-        contacts: contacts
+        onChat: null,
+        contacts: contacts,
+        status: function (message) {
+            return {
+                received: message.status === "received",
+                sent: message.status === "sent"
+            }
+        },
+        isActive: function (i) {
+            return {
+                active: i === this.onChat
+            }
+        }
     },
     methods: {
-        /*showWelcome: function () {
+        showWelcome: function () {
             let main = document.querySelector(".content > main");
 
             if (this.onChat == null) {
                 main.style.borderLeft = "1px solid #4adf83";
             }
             else {
-                main.style.borderLeft = "1px solid #000";
+                main.style.borderLeft = "1px solid #e4e5e6";
             }
-        }*/
+        }
     },
     created: function () {
-        /*this.showWelcome();*/
+        this.showWelcome();
+    },
+    updated: function () {
+        this.showWelcome();
     }
 });
